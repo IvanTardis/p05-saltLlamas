dfca76c356e40dca9af90cf3d7d161ebf0f0279d
Ivan Gontchar <ivan.tardis@outlook.com>
2025-05-09 11:47:33 -0400

    gamebuilder page in progress

---
+++ app/gameBuilder.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+import sqlite3
+import os
+
+def addEvent():
+    return 0
+
+def saveWork():
+    return 0
+
+def returnGame(name):
+    return 0
