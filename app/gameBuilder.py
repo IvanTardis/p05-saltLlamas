93bdcb511279c61ef7a2f5c6acc9ec52ffb4965b
Ivan Gontchar <ivan.tardis@outlook.com>
2025-05-15 11:27:00 -0400

    moving functionality over to seperate py file

--- app/gameBuilder.py
+++ app/gameBuilder.py
@@ -3,8 +3,14 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 import os
 
-def addEvent():
-    return 0
+currGame = []
+
+def getCurrGame():
+    return currGame
+
+def addEvent(input):
+    currGame.append(input)
+    return currGame
 
 def saveWork():
     return 0
