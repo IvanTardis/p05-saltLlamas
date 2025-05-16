a559f71f524e4bc15a4554836e1f36a1bef3c8af
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2025-05-16 15:42:50 +0000

    added layers but canvas issue

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,5 +1,32 @@
-const homebg = document.getElementByID('layer1')
+const homebg = document.getElementById('layer1')
 
 const ctx = homebg.getContext('2d')
-const img = document.getElementByID('hbg')
-ctx.drawImage(img, 10, 10)
+// const img = document.getElementByID('hbg')
+// ctx.drawImage(img, 10, 10)
+
+let speed = 2
+
+const bgl1 = new Image()
+bgl1.src = '../images/later-1.png'
+
+const bgl2 = new Image()
+bgl2.src = '../images/layer-2.png'
+
+const bgl3 = new Image()
+bgl3.src = './images/layer-3.png'
+
+const bgl4 = new Image()
+bgl4.src = './images/layer-4.png'
+
+const bgl5 = new Image()
+bgl5.src = './images/layer-5.png'
+
+const bgl6 = new Image()
+bgl6.src = './images/layer-6.png'
+
+function animate(){
+    ctx.drawImage(bgl1, 0, 0)
+    requestAnimationFrame(animate)
+}
+
+animate()
