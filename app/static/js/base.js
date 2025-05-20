d787f76268a99d95af319172232603738d2d4279
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2025-05-20 15:42:31 +0000

    images load

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,32 +1,28 @@
 const homebg = document.getElementById('layer1')
 
 const ctx = homebg.getContext('2d')
-// const img = document.getElementByID('hbg')
+// const img = document.getElementById('hbg')
 // ctx.drawImage(img, 10, 10)
 
 let speed = 2
 
-const bgl1 = new Image()
-bgl1.src = '../images/later-1.png'
+const bgl1 = document.getElementById('layer-1')
 
-const bgl2 = new Image()
-bgl2.src = '../images/layer-2.png'
+const bgl2 = document.getElementById('layer-2')
 
-const bgl3 = new Image()
-bgl3.src = './images/layer-3.png'
+const bgl3 = document.getElementById('layer-3')
 
-const bgl4 = new Image()
-bgl4.src = './images/layer-4.png'
+const bgl4 = document.getElementById('layer-4')
 
-const bgl5 = new Image()
-bgl5.src = './images/layer-5.png'
+const bgl5 = document.getElementById('layer-5')
 
-const bgl6 = new Image()
-bgl6.src = './images/layer-6.png'
+const bgl6 = document.getElementById('layer-6')
 
 function animate(){
+    console.log(bgl1.src)
     ctx.drawImage(bgl1, 0, 0)
     requestAnimationFrame(animate)
+
 }
 
 animate()
