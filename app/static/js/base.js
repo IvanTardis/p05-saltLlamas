7f0f3f4d7aed5edb7338f302552d9698e4243204
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2025-05-27 13:17:33 +0000

    got working parallax

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,36 +1,48 @@
-const homebg = document.getElementById('layer1')
+const canvas = document.getElementById('layer1');
+const ctx = canvas.getContext('2d');
 
-const ctx = homebg.getContext('2d')
-// const img = document.getElementById('hbg')
-// ctx.drawImage(img, 10, 10)
+//ordered from back (bottom) to front (top)
+const layers = [
+    { img: document.getElementById('layer-5'), speed: 0.01/2, y: 0 },
+    { img: document.getElementById('layer-4'), speed: 0.05/2, y: 0 },
+    { img: document.getElementById('layer-3'), speed: 0.1/2, y: 0 },
+    { img: document.getElementById('layer-6'), speed: 1.0/2, y: 0 }
+];
 
-let speed = 2
+const positions = layers.map(() => ({ x1: 0, x2: canvas.width }));
 
-const bgl1 = document.getElementById('layer-1')
-
-const bgl2 = document.getElementById('layer-2')
-
-const bgl3 = document.getElementById('layer-3')
-
-const bgl4 = document.getElementById('layer-4')
-
-const bgl5 = document.getElementById('layer-5')
-
-const bgl6 = document.getElementById('layer-6')
-
-let x = 0
-let xS = 2400
-
-function animate(){
-    ctx.clearRect(0,0, 800, 700)
-    ctx.drawImage(bgl1, x, 0)
-    ctx.drawImage(bgl2, xS, 0)
-    if (x < -2400) x = 2400 + xS - speed;
-    else x -= speed;
-    if (xS < -2400) xS = 2400 + x - speed;
-    else xS -= speed;
-    requestAnimationFrame(animate)
-
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    layers.forEach((layer, index) => {
+        const pos = positions[index];
+        
+        ctx.drawImage(layer.img, pos.x1, layer.y);
+        ctx.drawImage(layer.img, pos.x2, layer.y);
+        
+        pos.x1 -= layer.speed;
+        pos.x2 -= layer.speed;
+        
+        if (pos.x1 + canvas.width <= 0) {
+            pos.x1 = canvas.width;
+        }
+        if (pos.x2 + canvas.width <= 0) {
+            pos.x2 = canvas.width;
+        }
+    });
+    
+    requestAnimationFrame(animate);
 }
 
-animate()
+function resizeCanvas() {
+    canvas.width = 200;
+    canvas.height = 300;
+
+    layers.forEach((_, index) => {
+        positions[index] = { x1: 0, x2: canvas.width };
+    });
+}
+
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+animate();
