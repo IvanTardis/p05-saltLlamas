3c02b94e2b6c56962232b2be64fce6f382d73705
Christopher <clouie50@stuy.edu>
2025-06-05 22:05:55 -0400

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ SoftDev
 p05 - Le fin
 time spent: XYZ hrs
 '''
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
 from database import auth, createUser, getHighScore, build, updateImagePath
 from gameBuilder import *
 from gamePlayer import game_turn, load_game_state, save_game_state, game_state
